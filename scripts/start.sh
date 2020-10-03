bdcd7bf51072db7be1def1ed3abbeb1eaab0d69c
ich777 <ich777@noreply.example.org>
2020-10-03 13:54:05 +0200

    „scripts/start.sh“ ändern

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
+#!/bin/bash
+echo "---Checking if UID: ${UID} matches user---"
+usermod -u ${UID} ${USER}
+echo "---Checking if GID: ${GID} matches user---"
+usermod -g ${GID} ${USER}
 echo "---Setting umask to ${UMASK}---"
 umask ${UMASK}
 
 echo "---Checking for optional scripts---"
 if [ -f /opt/scripts/user.sh ]; then
 	echo "---Found optional script, executing---"
-	chmod +x /opt/scripts/user.sh
-	/opt/scripts/user.sh
+    chmod +x /opt/scripts/user.sh
+    /opt/scripts/user.sh
 else
 	echo "---No optional script found, continuing---"
 fi
