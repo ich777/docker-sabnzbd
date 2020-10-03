21a4f0f20ea537b3c2bc5568e4d0865032fa6ad4
ich777 <ich777@noreply.example.org>
2020-10-03 17:33:12 +0200

    „scripts/start-server.sh“ ändern

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-sleep infinity
 if [ "$SABNZBD_REL" == "latest" ]; then
     LAT_V="$(wget -qO- https://git.minenet.at/ich777/versions/raw/branch/master/SABnzbd | grep LATEST | cut -d '=' -f2)"
 elif [ "$SABNZBD_REL" == "prerelease" ]; then
